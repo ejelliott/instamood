386ced05418dcc38026c019d8da4f9cb2c2069fe
E.J. Elliott <ejelliott@MacBook-Pro-4.local>
2016-06-13 09:38:29 +0200

    instamood_v1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var API_DOMAIN = "https://api.instagram.com/v1";
 var RECENT_MEDIA_PATH = "/users/self/media/recent";
+var NUMBER_OF_PICS;
+var sentscores = 0;
+var caption_count = 0;
 // what do you think a variable in all caps means?
 
 $(document).ready(function() {
@@ -10,18 +13,149 @@ $(document).ready(function() {
   token = token.replace("#", "?"); // Prepare for query parameter
   var mediaUrl = API_DOMAIN + RECENT_MEDIA_PATH + token;
 
+  //Call the instagram api
   $.ajax({
     method: "GET",
     url: mediaUrl,
     dataType: "jsonp",
-    success: handleResponse,
+    success: function(response)
+      { 
+        handleResponse(response);
+        handleSentiment(response); 
+      },
     error: function() {
       alert("there has been an error...")
     }
-  })
-});
+  });
+}); 
 
 function handleResponse(response) {
   console.log(response);
-  // add stuff here!
+
+  //Get number of pictures
+  NUMBER_OF_PICS = response.data.length;
+
+  //Show the pictures with captions
+  for (var i = 0; i < response.data.length; i++) {
+    var link = response.data[i].images.standard_resolution.url;
+    var newImage = $("<img class=img-fluid img-center>").attr("src", link);
+    $("#list").append(newImage);
+    if (response.data[i].caption != null) {
+      var caption = $("<div class=caption></div>").html(response.data[i].caption.text)
+      $("#list").append(caption);
+    }
+  }
+
+  /* GENERATE STATS
+  * 1. EGO SCORE:
+  *   1) For each photo, see if the user has liked the photo
+  *   2) Divide the number of likes found by total number of
+  *      photos
+  */
+
+  var selfLikes = 0;
+  for (var i = 0; i < NUMBER_OF_PICS; i++) {
+    if (response.data[i].user_has_liked) 
+      selfLikes++;
+  }
+  var egoScore = Math.floor((selfLikes / NUMBER_OF_PICS) * 100);
+
+  
+  /* 2. POPULARITY SCORE:
+  *   1) Sum total number of likes from each photo
+  *   2) Divide sum by number of photos on page and return
+  *      average
+  */
+  var likeTotal = 0;
+  for (var i = 0; i < NUMBER_OF_PICS; i++) {
+    likeTotal += response.data[i].likes.count;
+  }
+  var popularityScore = Math.round(likeTotal / NUMBER_OF_PICS);
+
+  /* 3. ACTIVE DAYS:
+  *   1) Create an array of seven integers
+  *   2) for each pic, see which day it was posted on, 
+  *      and increment the corresponding integer
+  *   3) Return the most popular day
+  */
+
+  var days = [0, 0, 0, 0, 0, 0, 0];
+  var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  for (var i = 0; i < NUMBER_OF_PICS; i++) {
+    var time = parseInt(response.data[i].created_time);
+    var date = new Date(time * 1000);
+    days[date.getDay()]++;
+  }
+  var mostActiveDay = week[(Math.max.apply(Math, days))];
+
+  /* 4. BREVITY:
+  *   1) Sum the length of every caption
+  *   2) Divide by number of captions and return average
+  */
+  var captionLength = 0;
+  for (var i = 0; i < NUMBER_OF_PICS; i++) {
+    if (response.data[i].caption != null) {
+      captionLength += response.data[i].caption.text.length;
+    }
+  }
+  var brevity = Math.round(captionLength / NUMBER_OF_PICS);
+
+  /* 5. THIRST SCORE:
+  *   1) Sum the number of hashtags in all the pictures
+  *   2) Divide sum by number of pictures and return average
+  */
+  var hashtagCount = 0;
+  for (var i = 0; i < NUMBER_OF_PICS; i++) {
+      hashtagCount += response.data[i].tags.length;
+      // console.log(hashtagCount);
+  }
+  // var thirstScore = Math.round(hashtagCount / NUMBER_OF_PICS);
+  var thirstScore = (hashtagCount / NUMBER_OF_PICS);
+
+  //Add the stats to the page
+  $("#ego").append(egoScore);
+  $("#pop").append(popularityScore);
+  $("#days").append(mostActiveDay);
+  $("#brevity").append(brevity);
+  $("#thirst").append(thirstScore);
 }
+
+function handleSentiment (response) {
+  $.each(response.data, function(i, val) {
+      if (val.caption != null)
+        var caption = val.caption.text;
+      else caption = "";  
+      console.log(caption);  
+    // Make call to the sentiment API
+    var sentURL = "https://twinword-sentiment-analysis.p.mashape.com/analyze/"
+    $.ajax({
+      method: "POST",
+      url: sentURL,
+      headers: { "X-Mashape-Key": "vRCyNb1GSDmshH1wdJ4YRG5QM8oVp10uWfwjsnzere3HiJYfCq" },
+      data: { text : caption},
+      success: function (response) {
+        sentimentAnalysis(response);
+        console.log(response);
+      },
+      error: function(response) {
+        alert("there has been an error...");
+      }
+    });
+  });
+  /*var sentimentscore = Math.round(SENTSCORE / CAPTION_COUNT);
+  console.log(sentimentscore);
+  $("#sentiment").append(sentimentscore);*/
+}
+
+function sentimentAnalysis(data) {
+
+  // if (data.score != 0) {
+    // console.log(data);
+    // caption_count++;
+    // console.log(caption_count);
+    // sentscores += data.score;
+    // console.log(sentscores);
+  // }
+}
+
+
